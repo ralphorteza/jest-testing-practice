ce2e471befcd57f33eb882f44811ca6dbb3388c3
ralph orteza <ralphlorteza@gmail.com>
2023-01-20 10:11:56 -0800

    Modify sub function

--- src/calculator.js
+++ src/calculator.js
@@ -1,14 +1,22 @@
 const calc = (() => {
-  const add = function add(...args) {
-    const nums = args.filter((arg) => typeof arg === "number" );
-    console.log(`nums: ${nums} and num length: ${nums.length}`);
+  const add = function addition(...args) {
+    const nums = args.filter((arg) => typeof arg === "number");
+    // console.log(`nums: ${nums} and num length: ${nums.length}`);
     if (nums.length === 0) return null;
 
     return nums.reduce((accum, current) => (accum + current));
   }
 
+  const sub = function subtract(...args) {
+    const nums = args.filter((arg) => typeof arg === "number");
+    if (nums.length === 0) return null;
+
+    return nums.reduce((accum, current) => (accum - current));
+  }
+
   return {
-    add
+    add,
+    sub,
   };
 })();
 
