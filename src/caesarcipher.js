4e4aa7d18890d202f43779c10635b4a029eb7d9a
ralph orteza <ralphlorteza@gmail.com>
2023-01-21 10:18:56 -0800

    Refactor encrypt & encryptHandler functions

--- src/caesarcipher.js
+++ src/caesarcipher.js
@@ -3,8 +3,11 @@ const caesarCipher = function caesarCipherEncrypt() {
   // A helper function for encrypted. Takes the current character in the forEach loop
   // and pushes the encrypted character into the given array.
   const encryptHandler = function handlerForEncrypt(char, shifted, encryptedArr) {
-    const alphaChars = /[a-zA-z]/
+    const alphaChars = /[a-zA-z]/;
     const capitalChars = /[A-Z]/;
+    const MODULO_VALUE = 26;
+    const CAP_A_UTF_VALUE = 65;
+    const LOW_A_UTF_VALUE = 97;
 
     if (!char.match(alphaChars)) {
       encryptedArr.push(String.fromCharCode(char.charCodeAt(0)));
@@ -12,14 +15,14 @@ const caesarCipher = function caesarCipherEncrypt() {
     }
 
     if (char.match(capitalChars)) {
-      let charCode = (char.charCodeAt(0)) - 65;
-      let modifiedCharCode = ((charCode + shifted) % 26) + 65;
+      let charCode = (char.charCodeAt(0)) - CAP_A_UTF_VALUE;
+      let modifiedCharCode = ((charCode + shifted) % MODULO_VALUE) + CAP_A_UTF_VALUE;
       encryptedArr.push(String.fromCharCode(modifiedCharCode));
       return;
     }
 
-    let charCode = (char.charCodeAt(0)) - 97;
-    let modifiedCharCode = ((charCode + shifted) % 26) + 97;
+    let charCode = (char.charCodeAt(0)) - LOW_A_UTF_VALUE;
+    let modifiedCharCode = ((charCode + shifted) % MODULO_VALUE) + LOW_A_UTF_VALUE;
     encryptedArr.push(String.fromCharCode(modifiedCharCode));
   };
 
@@ -27,13 +30,15 @@ const caesarCipher = function caesarCipherEncrypt() {
   const encrypt = function encryptString(str, shift) {
     if (str === null || shift === null) return null;
     if (str === undefined || shift === undefined) return undefined;
-
-    const shifted = shift % 26;
+    
+    const MODULO_VALUE = 26;
+    const shifted = shift % MODULO_VALUE;
     const processedStr = str.trim();
     const charArray = [...processedStr];
     const encryptedArr = [];
-    charArray.forEach((char) =>  encryptHandler(char, shifted, encryptedArr));
 
+    charArray.forEach((char) =>  encryptHandler(char, shifted, encryptedArr));
+    
     return encryptedArr.join('');
   } 
 
