db42d4bc1bfb718282b3bd8054db0976015480e0
ralph orteza <ralphlorteza@gmail.com>
2023-01-20 16:46:23 -0800

    Test shifted number in caesarCipher.js

--- src/caesarcipher.js
+++ src/caesarcipher.js
@@ -1,31 +1,39 @@
 const caesarCipher = function caesarCipherEncrypt() {
 
-  const encryptHandler = function handlerForEncrypt(char, encryptedArr) {
-    const specialChars = /[$&+,:;=?@#|'<>.^*()%!-\s]/;
-    if (char.match(specialChars)) {
+  // A helper function for encrypted. Takes the current character in the forEach loop
+  // and pushes the encrypted character into the given array.
+  const encryptHandler = function handlerForEncrypt(char, shifted, encryptedArr) {
+    const alphaChars = /[a-zA-z]/
+    const capitalChars = /[A-Z]/;
+
+    if (!char.match(alphaChars)) {
       console.log('inside if statement!');
       encryptedArr.push(String.fromCharCode(char.charCodeAt(0)));
       return;
     }
-    if (char === /z/) {
-      encryptedArr.push('a')
+
+    if (char.match(capitalChars)) {
+      let charCode = (char.charCodeAt(0)) - 65;
+      let modifiedCharCode = ((charCode + shifted) % 26) + 65;
+      encryptedArr.push(String.fromCharCode(modifiedCharCode));
       return;
     }
 
-    let charCode = Number(char.charCodeAt(0));
-    charCode += 1;
-
-    encryptedArr.push(String.fromCharCode(charCode));
+    let charCode = (char.charCodeAt(0)) - 97;
+    let modifiedCharCode = ((charCode + shifted) % 26) + 97;
+    encryptedArr.push(String.fromCharCode(modifiedCharCode));
   };
 
-  const encrypt = function encryptString(str) {
-    if (str === null) return null;
-    if (str === undefined) return undefined;
+  // encrypt takes a string and an integer to to return an encrypted string.
+  const encrypt = function encryptString(str, shift) {
+    if (str === null || shift === null) return null;
+    if (str === undefined || shift === undefined) return undefined;
 
-    const processedStr = str.trim().toLowerCase();
+    const shifted = shift % 26;
+    const processedStr = str.trim();
     const charArray = [...processedStr];
     const encryptedArr = [];
-    charArray.forEach((char) =>  encryptHandler(char, encryptedArr));
+    charArray.forEach((char) =>  encryptHandler(char, shifted, encryptedArr));
 
     return encryptedArr.join('');
   } 
--- src/caesarcipher.test.js
+++ src/caesarcipher.test.js
@@ -3,8 +3,10 @@ const caesarCipher = require('./caesarcipher');
 
 test('null  to be null', () => {
   const str = null;
-  expect(caesarCipher.encrypt(null)).toBeNull();
-  expect(caesarCipher.encrypt(str)).toBeNull();
+  expect(caesarCipher.encrypt(null, 1)).toBeNull();
+  expect(caesarCipher.encrypt(str, 1)).toBeNull();
+  expect(caesarCipher.encrypt("abc", str)).toBeNull();
+  expect(caesarCipher.encrypt("abc", null)).toBeNull();
 });
 
 test('undefined to be undefined', () => {
@@ -13,23 +15,37 @@ test('undefined to be undefined', () => {
   expect(caesarCipher.encrypt(str)).toBeUndefined();
 });
 
-test('" " to be ""', () => {
-  expect(caesarCipher.encrypt(" ")).toBe("");
+test('" " shifted by 1 to be ""', () => {
+  expect(caesarCipher.encrypt(" ", 1)).toBe("");
 });
 
 
-test('"A" to be "b"', () => {
-  expect(caesarCipher.encrypt("A")).toBe("b");
+test('"A" shifted by 1 to be "b"', () => {
+  expect(caesarCipher.encrypt("A", 1)).toBe("B");
 });
 
-test('"abc" to be "bcd"', () => {
-  expect(caesarCipher.encrypt("abc")).toBe("bcd");
+test('"abc" shifted by 1 to be "bcd"', () => {
+  expect(caesarCipher.encrypt("abc", 1)).toBe("bcd");
 });
 
-test('"attack at dawn" to be "fyyfhp fy ifbs"', () => {
-  expect(caesarCipher.encrypt("attack at dawn")).toBe("buubdl bu ebxo");
+test('"attack at dawn" shifted by 1 to be "buubdl bu ebwo"', () => {
+  expect(caesarCipher.encrypt("attack at dawn", 1)).toBe("buubdl bu ebxo");
 });
 
-test('"Attack @ dawn." to be "fyyfhp @ ifbs."', () => {
-  expect(caesarCipher.encrypt("Attack @ dawn.")).toBe("buubdl @ ebxo.");
+test('"Attack @ daWn." shifted by 1 to be "Buubdl @ ebXo."', () => {
+  expect(caesarCipher.encrypt("Attack @ daWn.", 1)).toBe("Buubdl @ ebXo.");
+});
+
+test('"qrs tuv wxyz" shifted by 1 to be "vwx yza bcde"', () => {
+  expect(caesarCipher.encrypt("qrs tuv wxyz", 5)).toBe("vwx yza bcde");
+});
+
+test(`"Tincidunt! I'd aliquet, risus feugiat?" to be Qfkzfarkq! F'a xifnrbq, ofprp cbrdfxq?`, () => {
+  expect(caesarCipher.encrypt("Tincidunt! I'd aliquet, risus feugiat?", 23))
+  .toBe(`Qfkzfarkq! F'a xifnrbq, ofprp cbrdfxq?`);
+});
+
+test(`"Tincidunt! I'd aliquet, risus feugiat?" to be "Vkpekfwpv! K'f cnkswgv, tkuwu hgwikcv?"`, () => {
+  expect(caesarCipher.encrypt("Tincidunt! I'd aliquet, risus feugiat?", 28))
+  .toBe(`Vkpekfwpv! K'f cnkswgv, tkuwu hgwikcv?`);
 });
